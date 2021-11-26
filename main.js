1d1ca24bed8333739dd112084220aa62c2c8bbd1
Reda <redanabil2199@gmail.com>
2021-11-26 16:03:07 +0100

    tracker & animation update

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ import './styles/main.scss'
 const buttonCaroussel = document.querySelector('.carousselBtn'),
     sliderImages = document.querySelectorAll('.insideImg img'),
     sliderContainer = document.querySelector('.insideImg'),
-    slides = document.querySelectorAll('.carousselTextCard')
+    slides = document.querySelectorAll('.carousselTextCard'),
+    tracker = document.querySelectorAll('.trackerItem')
+
 let currentPicture = 0,
     slide
 
@@ -19,15 +21,24 @@ buttonCaroussel.addEventListener("click", (e) => {
     currentPicture++ //La valeur de currentPicture augmente de 1
     if (currentPicture >= sliderImages.length) {
         currentPicture = 0
-        sliderImages[currentPicture].style.opacity = 1; //Augmenter l'opacité de l'image du HomePod en fonction de la position
-        document.querySelector(".carousselTextCard:not(.noShow)").classList.add("noShow") // Classe noShow pour les éléments textes
+        sliderImages[currentPicture].style.opacity = 1;
+        document.querySelector(".carousselTextCard:not(.noShow)").classList.add("noShow")
         slides[currentPicture].classList.remove("noShow")
-        sliderImages[sliderImages.length - 1].style.opacity = 0; // Mettre l'opacité à 0 pour l'image du HomePod lorsqu'une autre est sélectionnée
+        document.querySelector(".trackerItem:not(.notExtended").classList.add("notExtended")
+        tracker[currentPicture].classList.remove("notExtended")
+        tracker[currentPicture].style.transition = "all 1s"
+
+        sliderImages[sliderImages.length - 1].style.opacity = 0;
+
     } else {
         sliderImages[currentPicture].style.opacity = 1;
         document.querySelector(".carousselTextCard:not(.noShow)").classList.add("noShow")
         sliderImages[currentPicture - 1].style.opacity = 0;
         slides[currentPicture].classList.remove("noShow")
+        document.querySelector(".trackerItem:not(.notExtended)").classList.add("notExtended")
+        tracker[currentPicture].classList.remove("notExtended")
+        tracker[currentPicture].style.transition = "all 1s"
+
     }
 })
 
